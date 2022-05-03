410e9a6ecbc5c86f11972915bbdb192182ecce93
megarex133 <binby133@gmail.com>
2022-05-03 15:51:08 +0300

    2 may

---
+++ app/src/main/java/com/example/anything/ProductList_activity.java
@@ -0,0 +1,14 @@
+package com.example.anything;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductList_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
---
+++ app/src/main/java/com/example/anything/Recepeis_activity.java
@@ -0,0 +1,14 @@
+package com.example.anything;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recepeis_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recepeis);
+    }
+}
