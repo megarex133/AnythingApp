52465106a996ea31808f1765fa5a08b74e39759c
megarex133 <binby133@gmail.com>
2022-05-06 18:37:31 +0300

    may 6

--- app/src/main/java/com/example/anything/MainActivity.java
+++ app/src/main/java/com/example/anything/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.anything;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLog = (Button) findViewById(R.id.buttonLog);
+
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProductList_activity.class));
+            }
+        });
     }
 }
