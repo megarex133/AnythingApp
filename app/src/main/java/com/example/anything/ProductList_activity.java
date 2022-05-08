82e63cae8a0d2c65469d1320508ff86c5f03bf12
megarex133 <binby133@gmail.com>
2022-05-08 14:25:07 +0300

    may 6

--- app/src/main/java/com/example/anything/ProductList_activity.java
+++ app/src/main/java/com/example/anything/ProductList_activity.java
@@ -5,25 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 public class ProductList_activity extends AppCompatActivity {
 
     ImageView searchIcon;
     EditText searchText;
 
-    TextView product_1;
-
-    ListView products;
+    ListView productList;
+String[] products = new String[]{"Apple","Lemon","Orange"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        productList = (ListView) findViewById(R.id.productList);
+        ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.product,products);
+
+        productList.setAdapter(adapter);
+
+        productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
 
         searchIcon = (ImageView) findViewById(R.id.searchIcon);
@@ -36,6 +50,10 @@ public class ProductList_activity extends AppCompatActivity {
                 searchText.setVisibility(View.VISIBLE);
             }
         });
+
+
+
+
 
     }
 }
