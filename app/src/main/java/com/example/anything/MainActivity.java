0cedeb2c46ebbbbc6b3d7c4c6948fa5231818b2e
megarex133 <binby133@gmail.com>
2022-05-08 12:57:55 +0300

    may 6

--- app/src/main/java/com/example/anything/MainActivity.java
+++ app/src/main/java/com/example/anything/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.anything;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonLog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, ProductList_activity.class));
+
             }
         });
+
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/anything/ProductList_activity.java
+++ app/src/main/java/com/example/anything/ProductList_activity.java
@@ -2,13 +2,40 @@ package com.example.anything;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ProductList_activity extends AppCompatActivity {
+
+    ImageView searchIcon;
+    EditText searchText;
+
+    TextView product_1;
+
+    ListView products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+
+        searchIcon = (ImageView) findViewById(R.id.searchIcon);
+        searchText = (EditText) findViewById(R.id.searchText);
+
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchText.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
