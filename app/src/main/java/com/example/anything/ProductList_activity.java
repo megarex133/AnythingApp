ad51ac11896adb87fc428b27f8fd1ef5d3d342ef
megarex133 <binby133@gmail.com>
2022-05-09 13:38:01 +0300

    may 9

--- app/src/main/java/com/example/anything/ProductList_activity.java
+++ app/src/main/java/com/example/anything/ProductList_activity.java
@@ -2,16 +2,19 @@ package com.example.anything;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ProductList_activity extends AppCompatActivity {
@@ -20,7 +23,8 @@ public class ProductList_activity extends AppCompatActivity {
     EditText searchText;
 
     ListView productList;
-String[] products = new String[]{"Apple","Lemon","Orange"};
+
+    Button findButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +32,15 @@ String[] products = new String[]{"Apple","Lemon","Orange"};
         setContentView(R.layout.activity_product_list);
 
         productList = (ListView) findViewById(R.id.productList);
-        ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.product,products);
-
+        productList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.products,R.layout.product);
         productList.setAdapter(adapter);
 
         productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Clicked" + item, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -51,6 +56,14 @@ String[] products = new String[]{"Apple","Lemon","Orange"};
             }
         });
 
+        findButton = (Button) findViewById(R.id.findButton);
+
+        findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProductList_activity.this,Recepeis_activity.class));
+            }
+        });
 
 
 
--- app/src/main/java/com/example/anything/Recepeis_activity.java
+++ app/src/main/java/com/example/anything/Recepeis_activity.java
@@ -3,12 +3,29 @@ package com.example.anything;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Recepeis_activity extends AppCompatActivity {
+
+    ImageView searchIcon;
+    EditText searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recepeis);
+
+        searchIcon = (ImageView) findViewById(R.id.searchIcon);
+        searchText = (EditText) findViewById(R.id.searchText);
+
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
