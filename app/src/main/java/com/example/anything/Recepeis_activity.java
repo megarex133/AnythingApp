0a79b48101a462ac13a86121438b31f14f57ecf2
megarex133 <binby133@gmail.com>
2022-05-12 20:49:36 +0300

    may 12

--- app/src/main/java/com/example/anything/Recepeis_activity.java
+++ app/src/main/java/com/example/anything/Recepeis_activity.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Recepeis_activity extends AppCompatActivity {
@@ -52,6 +54,23 @@ public class Recepeis_activity extends AppCompatActivity {
         // Функционал вывода рецептов
         mRecyclerView = (RecyclerView) findViewById(R.id.recipes);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(Recepeis_activity.this, 1);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+       recipesList = new ArrayList<>();
+        mRecipesData = new RecipesData("Boiled eggs","Eggs,salt","Boil eggs for 10 minutes",R.drawable.eggs);
+        recipesList.add(mRecipesData);
+        mRecipesData = new RecipesData("Borsch","Eggs,salt","Boil borsch",R.drawable.borsch);
+        recipesList.add(mRecipesData);
+        mRecipesData = new RecipesData("Borsch","Eggs,salt","Boil borsch",R.drawable.borsch);
+        recipesList.add(mRecipesData);
+        mRecipesData = new RecipesData("Borsch","Eggs,salt","Boil borsch",R.drawable.borsch);
+        recipesList.add(mRecipesData);
+        mRecipesData = new RecipesData("Borsch","Eggs,salt","Boil borsch",R.drawable.borsch);
+        recipesList.add(mRecipesData);
+
+        RecipeAdapter recipeAdapter = new RecipeAdapter(Recepeis_activity.this,recipesList);
+        mRecyclerView.setAdapter(recipeAdapter);
+        if(recipesList.isEmpty()== true) Toast.makeText(getApplicationContext(),"Recipes not found!", Toast.LENGTH_LONG).show();
 
 
 
